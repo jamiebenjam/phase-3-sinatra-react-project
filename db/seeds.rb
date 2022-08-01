b7082a4c4fa56c4bafee5938c9814d8d04f46e0e
Jamie <bennythedev@gmail.com>
2022-08-01 13:34:58 -0500

    model seed setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+#city
+c1 = City.create(name: "New York")
+c2 = City.create(name: "Chicago")
+
+#activities
+a1 = Activity.create(museum: "MOMA", restaurant: "Raos", park: "Central Park", misc: "shopping", city_id: c1.id)
+a2 = Activity.create(museum: "Art Institute", restaurant: "Giodornos", park: "Millenium Park", misc: "architecture tour", city_id: c2.id)
 
 puts "✅ Done seeding!"
