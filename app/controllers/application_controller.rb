c66075b46bce496227bf54d67cea96d4e2314115
Jamie <bennythedev@gmail.com>
2022-08-02 00:25:50 -0500

    get and post requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
+get "/" do
+  { message: "Good luck with your project!" }.to_json
+end
+
+get '/cities' do
+  cities = City.all
+  cities.to_json
+end
+
+get '/cities/:id' do
+    city = City.find(params[:id])
+    city.to_json
+end
+
+post '/cities' do
+    city = City.create(params)
+    city.to_json
+end
+
+# private
+
+# def city_params
+#   params.permit(:name)
+# end
+
 
 end
--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,26 +1,26 @@
 class CitiesController < ApplicationController
 
 # get
-get '/cities' do
-    cities = City.all
-    # { message: "cities" }.to_json
-    cities.to_json
-end
+# get '/cities' do
+#     cities = City.all
+#     # { message: "cities" }.to_json
+#     cities.to_json
+# end
 
 # dynamic routes
 
-get '/cities/:id' do
-    city = City.find(params[:id])
-    city.to_json
-end
+# get '/cities/:id' do
+#     city = City.find(params[:id])
+#     city.to_json
+# end
 
-# post
-post '/cities' do
-    # cities = City.all
-    # { message: "cities_post" }.to_json
-    city = City.create(city_params)
-    city.to_json
-    # cities.to_json
-end
+# # post
+# post '/cities' do
+#     # cities = City.all
+#     # { message: "cities_post" }.to_json
+#     city = City.create(city_params)
+#     city.to_json
+#     # cities.to_json
+# end
 
 end
